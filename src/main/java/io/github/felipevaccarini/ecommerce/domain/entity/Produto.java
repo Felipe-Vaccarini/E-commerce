0fee90fb0df13f78820e3dbf9cc154376692070b
felipe-vaccarini <felipe.jvp@hotmail.com>
2024-07-23 10:27:42 -0300

    Mapeando entidade produto

---
+++ src/main/java/io/github/felipevaccarini/ecommerce/domain/entity/Produto.java
@@ -0,0 +1,34 @@
+package io.github.felipevaccarini.ecommerce.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Size(max = 255)
+    @Column(name = "decricao", length = 255)
+    private String descricao;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Column(name = "preco", nullable = false)
+    private BigDecimal preco;
+
+    @NotNull
+    @PositiveOrZero
+    @Column(name = "estoque", nullable = false)
+    private Integer estoque;
+}
