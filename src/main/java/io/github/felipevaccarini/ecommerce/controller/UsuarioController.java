f2fd455a35085685276d966dbd7c6cdc0544b864
felipe-vaccarini <felipe.jvp@hotmail.com>
2024-07-26 11:46:02 -0300

    Implementado UsuarioController

---
+++ src/main/java/io/github/felipevaccarini/ecommerce/controller/UsuarioController.java
@@ -0,0 +1,47 @@
+package io.github.felipevaccarini.ecommerce.controller;
+
+import io.github.felipevaccarini.ecommerce.domain.entity.Usuario;
+import io.github.felipevaccarini.ecommerce.service.impl.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obterTodosUsuarios() {
+        List<Usuario> usuarios = usuarioService.findAll();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obterUsuarioPorId(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.findById(id);
+        return usuario
+                .map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioSalvo = usuarioService.save(usuario);
+        return ResponseEntity.ok(usuarioSalvo);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
