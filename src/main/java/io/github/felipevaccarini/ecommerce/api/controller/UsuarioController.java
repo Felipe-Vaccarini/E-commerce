1fe2f8a78584d7c98d2a8043656ed480fc8779ff
felipe-vaccarini <felipe.jvp@hotmail.com>
2024-08-14 15:54:56 -0300

    Ajustado UsuarioController, para utilizar DTO

--- src/main/java/io/github/felipevaccarini/ecommerce/api/controller/UsuarioController.java
+++ src/main/java/io/github/felipevaccarini/ecommerce/api/controller/UsuarioController.java
@@ -1,42 +1,53 @@
 package io.github.felipevaccarini.ecommerce.api.controller;
 
+import io.github.felipevaccarini.ecommerce.api.dto.UsuarioDTO;
 import io.github.felipevaccarini.ecommerce.domain.entity.Usuario;
 import io.github.felipevaccarini.ecommerce.service.impl.UsuarioService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UsuarioController(UsuarioService usuarioService) {
+    public UsuarioController(UsuarioService usuarioService, ModelMapper modelMapper) {
         this.usuarioService = usuarioService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> obterTodosUsuarios() {
+    public ResponseEntity<List<UsuarioDTO>> obterTodosUsuarios() {
         List<Usuario> usuarios = usuarioService.findAll();
-        return ResponseEntity.ok(usuarios);
+        List<UsuarioDTO> usuariosDTO = usuarios.stream()
+                .map(usuario -> modelMapper.map(usuario, UsuarioDTO.class))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(usuariosDTO);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> obterUsuarioPorId(@PathVariable Long id) {
+    public ResponseEntity<UsuarioDTO> obterUsuarioPorId(@PathVariable Long id) {
         Optional<Usuario> usuario = usuarioService.findById(id);
-        return usuario
-                .map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return usuario.map(value -> ResponseEntity.ok(modelMapper.map(value, UsuarioDTO.class)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<UsuarioDTO> criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
         Usuario usuarioSalvo = usuarioService.save(usuario);
-        return ResponseEntity.ok(usuarioSalvo);
+        UsuarioDTO usuarioSalvoDTO = modelMapper.map(usuarioSalvo, UsuarioDTO.class);
+        return ResponseEntity.ok(usuarioSalvoDTO);
     }
 
     @DeleteMapping("/{id}")
